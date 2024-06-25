4088926f8188f5f6f6f533a11784c66c70ae59d9
Lesn0y <kilril2101@gmail.com>
2024-06-25 15:21:53 +0300

    Refactoring

--- src/main/java/rest/RestService.java
+++ src/main/java/rest/RestService.java
@@ -15,40 +15,35 @@ public class RestService {
         restTemplate.getInterceptors().add(new CookieManager());
     }
 
-    public void start() {
-        // Step 1 - GET all users
+    public String getResultCode() {
         getAllUsers();
 
-        // Step 2 - POST new user
         User user = new User(3L, "James", "Brown", 23);
         String s1 = saveUser(user);
 
-        // Step 3 - PUT user with id = 3
         user.setName("Thomas");
         user.setLastName("Shelby");
         String s2 = updateUser(user);
 
-        //Step 4 - DELETE user with user = 3
         String s3 = deleteUser(3);
 
-        // Result
-        System.out.println("Result code: " + s1 + s2 + s3);
+        return s1 + s2 + s3;
     }
 
-    public User[] getAllUsers() {
+    private User[] getAllUsers() {
         return restTemplate
                 .getForObject(BASE_URL, User[].class);
     }
 
-    public String saveUser(User user) {
+    private String saveUser(User user) {
         return restTemplate.postForObject(BASE_URL, user, String.class);
     }
 
-    public String updateUser(User user) {
+    private String updateUser(User user) {
         return restTemplate.exchange(BASE_URL, HttpMethod.PUT, new HttpEntity<>(user) ,String.class).getBody();
     }
 
-    public String deleteUser(long id) {
+    private String deleteUser(long id) {
         return restTemplate.exchange(BASE_URL + "/" + id, HttpMethod.DELETE, null, String.class).getBody();
     }
 }
