1d7d26566f901df52d1f253a773ddc415e64e150
Lesn0y <kilril2101@gmail.com>
2024-06-25 11:59:38 +0300

    Add new method

--- src/main/java/rest/RestService.java
+++ src/main/java/rest/RestService.java
@@ -15,6 +15,26 @@ public class RestService {
         restTemplate.getInterceptors().add(new CookieManager());
     }
 
+    public void start() {
+        // Step 1 - GET all users
+        getAllUsers();
+
+        // Step 2 - POST new user
+        User user = new User(3L, "James", "Brown", 23);
+        String s1 = saveUser(user);
+
+        // Step 3 - PUT user with id = 3
+        user.setName("Thomas");
+        user.setLastName("Shelby");
+        String s2 = updateUser(user);
+
+        //Step 4 - DELETE user with user = 3
+        String s3 = deleteUser(3);
+
+        // Result
+        System.out.println("Result code: " + s1 + s2 + s3);
+    }
+
     public User[] getAllUsers() {
         return restTemplate
                 .getForObject(BASE_URL, User[].class);
